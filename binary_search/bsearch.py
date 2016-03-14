efb74633425763dee7a6abbebd34cc30dcecf2b9
Sergej Charskij <sergej.charskij@gmail.com>
2016-03-14 21:14:05 +0000

    returns the index of the element

--- binary_search/bsearch.py
+++ binary_search/bsearch.py
@@ -6,26 +6,24 @@ def binary_search_rec(n, lst, lBound, hBound):
         curVal = lst[curIn]
 
         if curVal == n:
-            return curVal
+            return curIn
         elif curVal > n:
             return binary_search_rec(n, lst, lBound, curIn - 1)
         else:
             return binary_search_rec(n, lst, curIn + 1, hBound)
 
 def binary_search(n, lst):
-    """Binary search algorithm
-    >>> binary_search(1, [1, 5 ,6, 9, 12])
-    1
-    >>> binary_search(12, [1, 5 ,6, 9, 12])
-    12
-    >>> binary_search(6, [1, 5 ,6, 9, 12])
-    6
-    >>> binary_search(7, [1, 5 ,6, 9, 12])
+    """Returns index of element in the list
+    >>> binary_search(1, [1, 5, 6, 9, 12])
+    0
+    >>> binary_search(12, [1, 5, 6, 9, 12])
+    4
+    >>> binary_search(6, [1, 5, 6, 9, 12])
+    2
+    >>> binary_search(7, [1, 5, 6, 9, 12])
     5
     """
     return binary_search_rec(n, lst, 0, len(lst) - 1)
-
-binary_search_rec(5, [1, 5 ,6, 9, 12], 0, 4)
 
 if __name__ == '__main__':
     import doctest
